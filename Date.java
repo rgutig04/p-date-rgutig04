a7094d60c2587d3ff0dabca27ced63522b502827
Ruben <rgutig04@estudiantes.unileon.es>
2017-03-27 12:14:23 +0200

    first commit

---
+++ Date.java
@@ -0,0 +1,7 @@
+public static void main(String args()){
+
+	Date fecha = new Date(27, 03, 2017);
+	System.out.println(fecha.isSameYear);
+	Date fecha2 = new Date (03, 01, 1985);
+	System.out.println(fecha2.isSame(fecha2));
+
